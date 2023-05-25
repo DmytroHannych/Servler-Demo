bd33271111dc679d47f3dff01e1b1190b2bd7dd3
Dmytro Hannych <you@example.com>
2023-05-26 00:10:53 +0300

    correct filter class

--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -9,16 +9,19 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.TimeZone;
+
+
+import static java.util.Objects.isNull;
 
 
 @WebFilter(value = "/time/*")
 public class TimezoneValidateFilter extends HttpFilter {
     @Override
     public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        TimeZone utc = TimeZone.getTimeZone("UTC");
+
+        String utc = "UTC";
         String timezone = req.getParameter("timezone");
-        if(timezone.contains(utc.toString())){
+        if( isNull(timezone) || timezone.contains(utc)){
             chain.doFilter(req,res);
         } else {
             res.setStatus(404);
